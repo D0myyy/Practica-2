06b5601aa1d9ad94897c59f08c3105579b7d0f70
D0myyy <tdumitruyt@gmail.com>
2026-05-05 09:20:32 +0300

    JavaFx

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Button btn = new Button("Click me!");
+
+        Scene scene = new Scene(btn, 300, 200);
+
+        stage.setTitle("My First JavaFX App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
